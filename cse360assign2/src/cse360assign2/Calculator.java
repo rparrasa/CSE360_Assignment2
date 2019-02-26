4a84f406fa2cd74852968c6a7104bd54ed3a96a4
Ricardo <Ricardo@LAPTOP-VKR7K9SU>
2019-02-25 21:04:35 -0700

    Second Version

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -3,32 +3,41 @@ package cse360assign2;
 public class Calculator {
 
 	private int total;
-	
+
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;	// returns total
 	}
 	
 	public void add (int value) {
-		
+	 total = value + total;  // add parameter to total
 	}
 	
 	public void subtract (int value) {
-		
+		total = total - value; // subtracts parameter from the total
 	}
 	
 	public void multiply (int value) {
-		
+		total = total * value; // Multiplies the parameter with the total
 	}
 	
 	public void divide (int value) {
+	
+		if (value == 0) // divides total by parameter and if parameter equals 0 then total equals 0.
+		{
+			total = 0;
 		
+		}
+		else 
+		{
+			total = total/value;
+		}
 	}
 	
 	public String getHistory () {
-		return "";
+		return ""; 
 	}
 }
--- cse360assign2/src/cse360assign2/Main.java
+++ cse360assign2/src/cse360assign2/Main.java
@@ -4,10 +4,15 @@ package cse360assign2;
 public class Main{
 	public static void main(String[] args) {
 	
-		System.out.println("hello world!!!");
+		System.out.println("hello world!!!\n");
 		
 		Calculator calc = new Calculator();
 		
+		calc.add(6);
+		calc.subtract(2);
+		calc.multiply(2);
+		calc.divide(4);
+		System.out.println(calc.getTotal());
 		  
 	}
 }
