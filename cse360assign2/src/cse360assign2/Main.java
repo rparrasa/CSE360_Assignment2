1e717c9b5e35a27eb6cec3a0685816040862580b
Ricardo <Ricardo@LAPTOP-VKR7K9SU>
2019-02-25 20:35:12 -0700

    Initial Start

---
+++ cse360assign2/src/cse360assign2/Main.java
@@ -0,0 +1,13 @@
+package cse360assign2;
+
+
+public class Main{
+	public static void main(String[] args) {
+	
+		System.out.println("hello world!!!");
+		
+		Calculator calc = new Calculator();
+		
+		  
+	}
+}
